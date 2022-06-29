4b9cf9944eec3c0e170eb7db88bec9ef55f5eb71
Nikolay <niktyler7@gmail.com>
2022-06-29 23:46:08 +0300

    Add modals.

--- dist/js/script.js
+++ dist/js/script.js
@@ -96,7 +96,22 @@
 "use strict";
 
 
-document.addEventListener('DOMContentLoaded', () => {});
+document.addEventListener('DOMContentLoaded', () => {//Modal
+  // //hamburger
+  // const burger = document.querySelector('.hamburger'),
+  //     right = document.querySelector('.navigation__links'),
+  //     itemRight = document.querySelectorAll('.navigation__link');
+  // burger.addEventListener('click', () => {
+  //     burger.classList.toggle('hamburger_active');
+  //     right.classList.toggle('navigation__links_active');
+  // });
+  // itemRight.forEach(item => {
+  //     item.addEventListener('click', () => {
+  //         burger.classList.toggle('hamburger_active');
+  //         right.classList.toggle('navigation__right_active')
+  //     });
+  // })
+});
 
 /***/ })
 
